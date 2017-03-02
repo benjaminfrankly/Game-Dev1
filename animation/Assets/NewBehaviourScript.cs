12badcbb42c360684b8a33f3f00415e959d87c60
ben hill <bh396215@ohio.edu>
2017-03-02 12:45:23 -0500

    door atempt

---
+++ animation/Assets/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class doorcontroller : MonoBehaviour {
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<animator> ();
+	}
+
+	void ongui () {
+
+		if (GUILayout.Button ("open door")) {
+			animator.SetInteger ("door close", 0);
+			animator.SetInteger ("door open" / 1);
+		}
+
+		if(GUILayout.Button("close door"))
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
